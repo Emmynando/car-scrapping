e4b97b946a302b89406c8448ceeb4472f78ea423
emmyxiano <alexander.emmyxiano@gmail.com>
2023-05-20 20:34:35 +0100

    fixed file content

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ import time
 # driver.close()
 
 
-class CarProductScrapper():
-    def __str__(self):
-        self.car_title = None
-        self.car_price = None
-        self.car_link = None
+class CarProductScrapper:
+    def __init__(self):
+        self.car_title = []
+        self.car_price = []
+        self. car_link = []
     
 
     def open_browser(self):
@@ -39,50 +39,46 @@ class CarProductScrapper():
         time.sleep(10)
     
     def get_product_title(self):
-        car_title =[]
-        # self.title = self.driver.find_elements(By.CLASS_NAME, "vehicle-card_detailsSection__o1SvU")
-        self.title = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_detailsSection__o1SvU']/span/p")
-        for content_title in self.title:
-        # content_title = "".join([str(element.text) for element in self.title])
-            car_title.append(content_title.text)
-            yield(car_title)
+        elements = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_detailsSection__o1SvU']/span/p")
+        for content_title in elements:
+            if content_title in self.car_title:
+                continue
+            self.car_title.append(content_title.text)
+        return(self.car_title)
     
 
-        # time.sleep(5)
-    # def get_product_price(self):
-    #     car_price =[]
-    #     self.price = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_priceSection__knSHH']//h2")
-    #     for content_price in self.price:
-        # content_price = "".join([element.text for element in self.price])
-            # car_price.append(content_price.text)
-            # yield(car_price)
-
-    #     time.sleep(5)
+    def get_product_price(self):
+        self.price = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_priceSection__knSHH']//h2")
+        for content_price in self.price:
+            if content_price in self.car_price:
+                continue
+            content_price = "".join([element.text for element in self.price])
+            self.car_price.append(content_price)
+        return(content_price)
     
-    # def get_product_link(self):
-    #     car_link =[]
-    #     self.link = self.driver.find_elements(By.XPATH, "//section[@class='vehicle-card_vehicleCardContainer___2DV3']/a")
-    #     for x in self.link:
-    #         car_link.append(x.get_attribute('href'))
-    #         yield(car_link)
+    def get_product_link(self):
+        self.link = self.driver.find_elements(By.XPATH, "//section[@class='vehicle-card_vehicleCardContainer___2DV3']/a")
+        for x in self.link:
+            if x in self.car_link:
+                continue
+            self.car_link.append(x.get_attribute('href'))
+        return(self.car_link)
 
         time.sleep(5)
         self.driver.close()
     
-    # def product_file(self, car_title, car_price, car_link):
-    #     filename = 'products.csv'
-    #     file = open(filename, "w", newline='')
-    #     reader = csv.writer(file, delimiter='\t')
-    #     reader.writerow(['Car Name', 'Car Price', 'Car Link'])
-    #     for row in zip(car_title, car_price, car_link):
-    #         reader.writerows(row)
-        # reader.writerows(zip([car_title, car_price, car_link]))
-        # file.close()
+    def product_file(self, car_title, car_price, car_link):
+        filename = 'products.csv'
+        file = open(filename, "w", newline='')
+        reader = csv.writer(file, delimiter='\t')
+        reader.writerow(['Car Name', 'Car Price', 'Car Link'])
+        reader.writerows(zip([car_title, car_price, car_link]))
+        file.close()
 
-        # message = f">> Information about the car product is stored in {filename}\n"
-        # print(message)
+        message = f">> Information about the car product is stored in {filename}\n"
+        print(message)
 
-        # os.startfile(filename)
+        os.startfile(filename)
 
 
 
@@ -90,25 +86,12 @@ if __name__ == '__main__':
     car = CarProductScrapper()
     car.open_browser()
     product_title = car.get_product_title()
-    # print(product_title)
-    # product_price = car.get_product_price()
-    # product_link = car.get_product_link()
-    # car.product_file(product_title, product_price, product_link)
+    product_price = car.get_product_price()
+    product_link = car.get_product_link()
+    print(type(product_price))
+    car.product_file(product_title, product_price, product_link)
 
 
-
-            # x.get_attribute('href')
-            # car_link.append[x]
-            # print(type(x))
-            # print(x.get_attribute('href'))
-# try:
-#     element = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.NAME, "Used"))
-#     )
-#     element.click()
-# except:
-#     driver.quit()
-# search.send_keys(Keys.RETURN)
 
 
 
