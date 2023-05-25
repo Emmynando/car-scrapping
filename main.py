bdf098b439babe63319591259952e249b8718782
Kai Striega <kaistriega@gmail.com>
2023-05-25 17:50:26 +0800

    Simplify `get_product_*` methods
    These methods were overcomplicated, instead of
    trying to access an internal list, return the list
    directly

--- main.py
+++ main.py
@@ -34,10 +34,6 @@ class CarProductScrapper:
         # dictionary and call it with `self._service`.
         self.driver = self._browser_to_driver_mapper[browser_type](service=self.service)
 
-        self.car_title = []
-        self.car_price = []
-        self.car_link = []
-
     def open_browser(self):
         self.driver.maximize_window()
         self.driver.get('https://my.caredge.com/buy?inventoryType=used&priceRange=-16001&sortBy=dist&sortOrder=asc&start=0')
@@ -45,51 +41,40 @@ class CarProductScrapper:
 
     def get_product_title(self):
         elements = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_detailsSection__o1SvU']/span/p")
-        for content_title in elements:
-            if content_title.text in self.car_title:
-                continue
-            self.car_title.append(content_title.text)
-        return self.car_title
+        return [content_title.text for content_title in elements]
 
     def get_product_price(self):
         elements = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_priceSection__knSHH']//h2")
-        for content_price in elements:
-            if content_price.text in self.car_price:
-                continue
-            self.car_price.append(content_price.text)
-        return self.car_price
+        return [content_price.text for content_price in elements]
 
     def get_product_link(self):
-        self.link = self.driver.find_elements(By.XPATH, "//section[@class='vehicle-card_vehicleCardContainer___2DV3']/a")
-        for x in self.link:
-            if x in self.car_link:
-                continue
-            self.car_link.append(x.get_attribute('href'))
+        elements = self.driver.find_elements(By.XPATH, "//section[@class='vehicle-card_vehicleCardContainer___2DV3']/a")
+        return [element.get_attribute("href") for element in elements]
+
+    def close_browser(self):
         time.sleep(5)
         self.driver.close()
-        return self.car_link
 
-    def product_file(self, car_title, car_price, car_link):
-        filename = 'products.csv'
-        with open(filename, "w", newline='') as file:
-            file = open(filename, "w")
-            writer = csv.writer(file, delimiter='\t')
-            writer.writerow(['Car Name', 'Car Price', 'Car Link'])
-            writer.writerows(zip([car_title, car_price, car_link]))
-            file.close()
 
-            message = f">> Information about the car product is stored in {filename}\n"
-            print(message)
+def write_car_data_to_csv(file_name, car_data):
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Car Name", "Car Price", "Car Link"])
+        writer.writerows(car_data)
+        print(f">> Information about the car product is stored in {file_name}")
 
 
 if __name__ == '__main__':
     # `browser_type` and `service_path` vary depending on your setup.
     car = CarProductScrapper(service_path=Path.cwd(), browser_type=BrowserType.FIREFOX)
     car.open_browser()
-    product_title = car.get_product_title()
-    product_price = car.get_product_price()
-    product_link = car.get_product_link()
-    car.product_file(product_title, product_price, product_link)
+    product_titles = car.get_product_title()
+    product_prices = car.get_product_price()
+    product_links = car.get_product_link()
+    car.close_browser()
+
+    zipped_data = zip(product_titles, product_prices, product_links)
+    write_car_data_to_csv("products.csv", zipped_data)
 
 
 
