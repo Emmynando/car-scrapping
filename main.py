067b987d0e4358b792c4fc06ed3eece0395bea9b
Kai Striega <kaistriega@gmail.com>
2023-05-25 16:57:39 +0800

    Tidy up file
    * Remove unused imports
    * Clean up whitespace
    * Remove extra blank lines

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
 import csv
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
-
-# PATH ='C:\Program Files (x86)\chromedriver.exe'
-
-# service_obj = Service("C:\Program Files (x86)\chromedriver.exe")
-# driver = webdriver.Chrome(service=service_obj)
-# driver.maximize_window()
-# driver.get('https://my.caredge.com/buy?inventoryType=used&priceRange=-16001&sortBy=dist&sortOrder=asc&start=0')
-# time.sleep(5)
-# title = driver.find_element(By.CLASS_NAME, "vehicle-card_detailsSection__o1SvU")
-# title = driver.find_elements(By.XPATH, "//div[@class='vehicle-card_detailsSection__o1SvU']")
-# content = "".join([element.text for element in title])
-# print(content)
-
-# time.sleep(5)
-# driver.close()
 
 
 class CarProductScrapper:
     def __init__(self):
         self.car_title = []
         self.car_price = []
-        self. car_link = []
-    
+        self.car_link = []
 
     def open_browser(self):
         self.service_obj = Service("C:\Program Files (x86)\chromedriver.exe")
@@ -37,7 +18,7 @@ class CarProductScrapper:
         self.driver.maximize_window()
         self.driver.get('https://my.caredge.com/buy?inventoryType=used&priceRange=-16001&sortBy=dist&sortOrder=asc&start=0')
         time.sleep(10)
-    
+
     def get_product_title(self):
         elements = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_detailsSection__o1SvU']/span/p")
         for content_title in elements:
@@ -45,7 +26,6 @@ class CarProductScrapper:
                 continue
             self.car_title.append(content_title.text)
         return self.car_title
-    
 
     def get_product_price(self):
         elements = self.driver.find_elements(By.XPATH, "//div[@class='vehicle-card_priceSection__knSHH']//h2")
@@ -54,7 +34,7 @@ class CarProductScrapper:
                 continue
             self.car_price.append(content_price.text)
         return self.car_price
-    
+
     def get_product_link(self):
         self.link = self.driver.find_elements(By.XPATH, "//section[@class='vehicle-card_vehicleCardContainer___2DV3']/a")
         for x in self.link:
@@ -65,7 +45,6 @@ class CarProductScrapper:
         self.driver.close()
         return self.car_link
 
-    
     def product_file(self, car_title, car_price, car_link):
         filename = 'products.csv'
         with open(filename, "w", newline='') as file:
@@ -77,9 +56,6 @@ class CarProductScrapper:
 
             message = f">> Information about the car product is stored in {filename}\n"
             print(message)
-
-
-
 
 
 if __name__ == '__main__':
